5be65ac52c14d7379f99c5ed7f5d7f50bd63cdac
hsinhui <shinhuei1@gmail.com>
2024-05-04 19:10:23 +0800

    feat: add button component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,19 @@ const config = {
         "2xl": "1400px",
       },
     },
+    spacing: {
+      1: "4px",
+      2: "8px",
+      3: "12px",
+      4: "16px",
+      5: "20px",
+      6: "24px",
+      7: "28px",
+      8: "32px",
+      9: "36px",
+      10: "40px",
+      11: "44px",
+    },
     extend: {
       colors: {
         border: "hsl(var(--border))",
