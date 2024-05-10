0213412a130075e7ee54ff09eccf45004f1abd09
hsinhui <shinhuei1@gmail.com>
2024-05-10 21:28:15 +0800

    feat: login feature

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -2,9 +2,13 @@ import type { ReactNode } from 'react';
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex">
-      <div className="w-2/5 h-screen hidden md:block bg-cover bg-center bg-[url('../images/auth-page-bg.webp')]" />
-      <>{children}</>
+    <div className="flex h-screen">
+      <div className="w-2/5 hidden md:block bg-cover bg-center bg-[url('../images/auth-page-bg.webp')]" />
+      <div className="flex-1 pt-[70px] overflow-y-auto">
+        <div className="max-w-sm mx-auto pt-14">
+          {children}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/custom/InputField.tsx
@@ -0,0 +1,43 @@
+import type { HTMLInputTypeAttribute } from 'react';
+import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '../ui/form';
+import { Input } from '../ui/input';
+import type { Control, FieldValues } from 'react-hook-form';
+import type { LucideIcon } from 'lucide-react';
+
+type InputFieldProps = {
+  control: Control<FieldValues>;
+  name: string;
+  label: string;
+  inputType?: HTMLInputTypeAttribute;
+  placeholder?: string;
+  formDescription?: string;
+  startIcon?: LucideIcon;
+  endIcon?: LucideIcon;
+  iconAction?: () => void;
+}
+
+export default function InputField ({ control, name, label, inputType, placeholder, formDescription, startIcon, endIcon, iconAction }: InputFieldProps) {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>{label}</FormLabel>
+        <FormControl>
+            <Input
+              type={inputType || 'text'}
+              placeholder={placeholder || ''}
+              startIcon={startIcon}
+              endIcon={endIcon}
+              iconAction={iconAction}
+              {...field}
+            />
+          </FormControl>
+          {formDescription && <FormDescription>{formDescription}</FormDescription>}
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+}
