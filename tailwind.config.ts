9fae089ea2b19deb120f62e796e871b15f2740bd
enzo.luo <duenzo1010@gmail.com>
2024-06-02 09:23:05 +0800

    chore: eslint for tailwindcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -31,7 +31,7 @@ const config = {
     },
     extend: {
       borderWidth: {
-        '10': '10px'
+        '10': '10px',
       },
       colors: {
         border: 'hsl(var(--border))',
@@ -110,20 +110,20 @@ const config = {
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
       screens: {
-        'xs': { 'min': '375px', 'max': '639px' },
-        'sm': { 'min': '640px', 'max': '767px' },
-        'md': { 'min': '768px', 'max': '1023px' },
-        'lg': { 'min': '1024px', 'max': '1279px' },
-        'xl': { 'min': '1280px', 'max': '1534px' },
-        '2xl': { 'min': '1535px' },
+        xs: { min: '375px', max: '639px' },
+        sm: { min: '640px', max: '767px' },
+        md: { min: '768px', max: '1023px' },
+        lg: { min: '1024px', max: '1279px' },
+        xl: { min: '1280px', max: '1534px' },
+        '2xl': { min: '1535px' },
       },
       maxWidth: {
         '1024': '1024px',
         '1280': '1280px',
       },
       boxShadow: {
-        tabs: '0px 2px 8px 0px rgba(39, 41, 55, 0.08)'
-      }
+        tabs: '0px 2px 8px 0px rgba(39, 41, 55, 0.08)',
+      },
     },
   },
   plugins: [require('tailwindcss-animate')],
